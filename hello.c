89bef47c39cc9a330037d79c16499ce2aa79c5ea
tangyuehao <1019673236@qq.com>
2020-08-17 21:42:41 +0800

    First commit

---
+++ hello.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "add.h"
+float add(float a, float b);
+
+int main(int argc, char *argv[])
+{
+	float x,y,z;
+	
+	if(argc == 3){
+		y = atof(argv[1]);
+		z = atof(argv[2]);
+		x = add(y, z);
+		printf("%f\n",x);
+	}
+	else{
+		printf("!");
+		return 1;
+	}
+	return 0;
+}
